50bea4aac9b1cc42ec34216359785117854bc98b
Israel-199 <israelassefa199@gmail.com>
2026-01-06 06:16:16 -0800

    making connect the exam section with backend

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,4 +1,4 @@
-// import { Navbar } from "@/components/Navbar";
+
 import { HeroSection } from "@/components/HeroSection";
 import { ServicesSection } from "@/components/ServicesSection";
 import  ExamsSection  from "../components/admin/ExamSection";
