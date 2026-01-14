bf14330728c72a94dc3bdd87e0b0f902c5a2b7a5
Israel-199 <israelassefa199@gmail.com>
2026-01-14 02:12:24 -0800

    make it render not to sleep

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,10 @@ const allowedOrigins = [
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
+const axios = require("axios"); 
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -31,17 +30,24 @@ app.use(cors({
   credentials: true,
 }));
 
-
-
 app.use("/api/exams", examRoutes);
 app.use("/api/testimonials", testimonialRoutes);
 app.use("/api/blogs", blogRoutes);
 app.use("/api/resources", resourceRoutes);
 app.use("/api/admin", adminRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Service is alive!");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
+  setInterval(() => {
+    axios.get("https://nova-exams.onrender.com/")
+      .then(() => console.log("Pinged self to stay awake"))
+      .catch(err => console.error("Ping failed", err.message));
+  }, 14 * 60 * 1000);
 });
