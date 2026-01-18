4fea8a343db199df46d47599e1340aa66ef09d74
Israel-199 <israelassefa199@gmail.com>
2026-01-18 10:44:32 -0800

    adding the terms routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -51,7 +51,7 @@ const App = () => (
             <Route path="/contact-us" element={<Contact />} />
             <Route path="/booking" element={<Booking />} />
             <Route path="/privacy" element={<PrivacyPage />} />
-
+            <Route path="/terms" element={<TermsPage />} />
             <Route
               path="/admin"
               element={
